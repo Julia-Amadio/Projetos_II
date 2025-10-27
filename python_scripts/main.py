b05414ad62f465780eee8adc23f1de91abd6ad71
Julia-Amadio <98353170+Julia-Amadio@users.noreply.github.com>
2025-10-27 15:23:45 -0300

    Upload main.py, update requirements

---
+++ python_scripts/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, File, UploadFile
+from feature_extractor_single import process_single_image
+import shutil
+import os
+
+app = FastAPI(title="SojaFeatExtractorAPI")
+
+@app.post("/extract_features/")
+async def extract_features(file: UploadFile = File(...)):
+    """
+    Endpoint que recebe uma imagem e retorna o caminho
+    do vetor de características extraído.
+    """
+    temp_path = f"temp_{file.filename}"
+    with open(temp_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    #Processa a imagem (segmentação + extração)
+    features_path = process_single_image(temp_path)
+
+    #Remove imagem temporária
+    os.remove(temp_path)
+
+    return {"features_saved_at": features_path}
